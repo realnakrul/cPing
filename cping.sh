e9cca273844768a7a3d404e0eeb0692a9077d4e2
realnakrul <na_krul@mail.ru>
2018-07-19 12:53:23 +0400

    Initial release. TODO: Statistics, repeat count from CLI: case + shift

--- cping.sh
+++ cping.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-while true
-  do
-    DATETIME=$(date)
-    if [[ ${DATETIME} == *:00\ * ]]
-    then
-      echo -e
-      echo -ne ${DATETIME}
-      echo " Pinging $1"
-    fi
+rep=5
+while [ $rep -gt 0 ]; do
     ping -c 1 -w 1 $1 -q 2>&1 >/dev/null
-    RET=$?
-    if [ ${RET} -eq 0 ]; then
+    RES=$?
+    if [ ${RES} -eq 0 ]; then
       echo -ne '!'
     else
       echo -ne '.'
     fi
-    sleep 1
+    sleep 0.05
+    let rep=($rep-1)
+#    echo $rep
 done
+echo ''
