6a2fa612391e341f6012f975d740215526d5c9c5
realnakrul <na_krul@mail.ru>
2018-07-19 14:39:37 +0400

    Back to While loop. Size 100 bytes. TODO: Statistics, repeat count from CLI: case + shift

--- cping.sh
+++ cping.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-REP=5
-echo 'Sending '$REP', 100-byte ICMP Echos to '$1', timeout is 1 seconds:'
-for i in {1..5}; do
-    ping -c 1 -w 1 $1 -q 2>&1 >/dev/null
-    RES=$?
-    if [ ${RES} -eq 0 ]; then
+REPEAT=5
+SUCCESS=0
+SIZE=100
+PERCENT=0
+echo 'Sending '$REPEAT', '$SIZE'-byte ICMP Echos to '$1', timeout is 2 seconds:'
+i=1
+while [ $i -le $REPEAT ]; do
+    ping -c 1 -w 2 -W 2 -s $SIZE $1 -q 2>&1 >/dev/null
+    RESULT=$?
+    if [ ${RESULT} -eq 0 ]; then
       echo -ne '!'
+      let SUCCESS=($SUCCESS+1)
     else
       echo -ne '.'
     fi
     sleep 0.05
+    i=$(($i + 1))
 #    let rep=($rep-1)
 #    echo $REP
 done
 echo ''
-echo 'Success rate is 100 percent (5/5), round-trip min/avg/max = 7/14/21 ms'
+let PERCENT=$(( $SUCCESS*100/$REPEAT ))
+echo 'Success rate is '$PERCENT' percent ('$SUCCESS'/'$REPEAT'), round-trip min/avg/max = RTT_MIN/RTT_AVG/RTT_MAX ms'
